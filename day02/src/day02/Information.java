904f64706e8d4abca0f6a6ddf98a1beea41ee15c
smilesyk0323 <smilesyk0323@naver.com>
2023-06-20 10:56:27 +0900

    파일 생성및 출력프로그램 생성 테스트

---
+++ day02/src/day02/Information.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class Information {
+	public static void main(String[] args) {
+		System.out.println("공공데이터 융합 웹 애플리케이션 개발자 양성과정4");
+		System.out.println("황인빈");
+		System.out.println("신윤경");
+	}
+
+}

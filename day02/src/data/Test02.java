f8e91f3071bb71b78e64fba394521198946791d8
smilesyk0323 <smilesyk0323@naver.com>
2023-06-20 12:42:40 +0900

    잘못된 예시와 올바른예시

---
+++ day02/src/data/Test02.java
@@ -0,0 +1,16 @@
+package data;
+
+public class Test02 {
+	public static void main(String[]args){
+		//잘못된 예시1
+		//System.out.println(38000);
+		
+		//잘못된 예시2
+		//System.out.println(14000+24000)';
+		
+		//올바른예시
+		System.out.println(7000*2);
+		System.out.println(8000*2);
+		System.out.println(7000*2+8000*3);
+	}
+}
---
+++ day02/src/data/Test03.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Test03 {
+	public static void main(String[]args) {
+		//변수(Variable) = 데이터 기억장치 
+		
+		
+	}
+
+}

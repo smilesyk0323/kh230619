fd4e6cf8168303b8c57e3675c5accd340dc90b5a
smilesyk0323 <smilesyk0323@naver.com>
2023-06-20 17:46:49 +0900

    과제풀이 완료

--- day02/src/data/Test13.java
+++ day02/src/data/Test13.java
@@ -6,5 +6,24 @@ public class Test13 {
 		//우리반 학생27명과 강사까지 마실 커피를 주문했을 때 다음 항목을 계산하여 출력하세요
 		//*무료로 계산되는 커피 잔 수 * 최종 결제 금액 
 		//커피는 한 잔에 2500원 입니다. 
+		int student = 27;
+		int teacher = 1;
+		int coffee = 2500;
+		int ourClass = student + teacher;
+		
+		int freeCount = ourClass / 5;
+	
+		int payCount = ourClass - freeCount;
+		int payMoney = payCount * coffee;
+		
+		System.out.println(freeCount);
+		System.out.println(payMoney);
+	
+				
+				
+				
+				
+				
+				
 	}
 }

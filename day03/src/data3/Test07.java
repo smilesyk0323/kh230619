3e8330c1f2cbca947b4c42d6f2bad58b4e54014a
smilesyk0323 <smilesyk0323@naver.com>
2023-06-22 09:54:14 +0900

    day04 생성

--- day03/src/data3/Test07.java
+++ day03/src/data3/Test07.java
@@ -12,16 +12,40 @@ public class Test07 {
 		//-400의 배수인 해는 100의 배수일지라도 윤년으로 간주합니다
 		//이 기준을 토대로 특정 연도가 윤년인지 아닌지 판정해서 출력 
 		
-		int year = 2400;
+		//int year = 2000;
 		
 		//int 윤년 = 4의 배수 && 4의배수 - (100의 배수-400의 배수) 
 		
-		boolean year4 = year % 400 ==0; 
-		boolean year1 = year % 100 == 0;
-		boolean yYear = year % 4 == 0 &&  (year1 == year4); 
+		//boolean year4 = year % 400 ==0; 
+		//boolean year1 = year % 100 == 0;
+		//boolean yYear = year % 4 == 0 &&  (year1 == year4); 
 		
-		System.out.println(yYear);
+		//System.out.println(yYear);
 				
+		
+		//윤년 계산 프로그램 풀이
+		
+		//입력
+		int year = 2000;
+				
+		//계산
+		//[1] 4의 배수면 윤년                       include
+		//[2] 100의 배수면 윤년이 아니다       exclude
+		//[3] 400의 배수면 윤년이다             include
+		
+		//boolean leepYear = 3 || 2 && 1;  //조건3이 가장 앞으로 온 이유!! 중요!
+		
+		boolean is4 = year % 4 == 0;
+		//boolean isNot100 = year % 100 != 0;
+		boolean isNot100 = year % 100 != 0;
+		boolean is400 = year % 400 == 0;
+		boolean leapYear = is400 || is4 && isNot100;
+					
+				
+		//출력
+		System.out.println(leapYear);
+		
+		
 				
 	}
 }
---
+++ day04/src/data4/Test01.java
@@ -0,0 +1,5 @@
+package data4;
+
+public class Test01 {
+
+}

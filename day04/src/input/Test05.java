c87d8750e8e27bd357885aed0f33bf70ce7514f9
smilesyk0323 <smilesyk0323@naver.com>
2023-06-22 15:37:58 +0900

    분식집 금액 계산기 예제 풀이

--- day04/src/input/Test05.java
+++ day04/src/input/Test05.java
@@ -9,29 +9,43 @@ public class Test05 {
 		//사용자에게 떡볶이, 튀김, 순대 개수를 입력받아 결재금액을 출력
 		//단 현재 행사중이라서 10% 할인된 가격으로 판매합니다
 		
+		//분식점 금액 계산 풀이
+		
+		//입력 
+		int menu1Price = 3000;
+		int menu2Price = 500;
+		int menu3Price = 2000;
+		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("떡볶이 주문:");
-		int ddCount = sc.nextInt();
-		System.out.print("튀김 주문:");
-		int ttCount = sc.nextInt();
-		System.out.print("순대 주문:");
-		int ssCount = sc.nextInt();
+		System.out.print("떡볶이(3000원): ");
+		int menu1Count = sc.nextInt();
+		System.out.print("튀김(500원):");
+		int menu2Count = sc.nextInt();
+		System.out.print("순대(2000원):");
+		int menu3Count = sc.nextInt();
 		
 		sc.close();
 		
-		int dd = 3000;
-		int tt = 500;
-		int ss = 2000;
+		int rate = 10;
 		
-		int total = (dd * ddCount) + (tt * ttCount) + (ss * ssCount);
+		//계산 
+		int menu1Total = menu1Price * menu1Count;
+		int menu2Total = menu2Price * menu2Count;
+		int menu3Total = menu3Price * menu3Count;
 		
-		int discountPrice = total - (total * 10 / 100);
+		int total = menu1Total + menu2Total + menu3Total;
+	
+		int discount = total * rate / 100;
+		int result = total - discount;
 		
-		//System.out.println(discountPrice);
+		//출력
+		System.out.println("<결제 정보>");
+		System.out.println("결제 금액 : " + total +"원");
+		System.out.println("할인금액: " + discount + "원");
+		System.out.println("최종 금액:" + result + "원");
 		
-		System.out.println("총결제금액은"+ discountPrice+"입니다." );
-
+		
 				
 
 	}

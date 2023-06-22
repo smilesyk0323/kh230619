a362360ae4ce4940895926b6f31101b7d754efd9
smilesyk0323 <smilesyk0323@naver.com>
2023-06-22 18:24:09 +0900

    여행프로모션2 예제풀이

---
+++ day04/src/condition/Test04_1.java
@@ -0,0 +1,32 @@
+package condition;
+
+import java.util.Scanner;
+
+public class Test04_1 {
+	public static void main(String[]args) {
+		//KH여행사에서 두 번째 프로모션을 다음과 같이 진행하려고 합니다
+		//-여름 매출이 저조하여 여름에 여행갈 경우 비용에서 25%를 할인합니다
+		//- 여름이 아닌 다른 계절은 할인 행사를 진행하지 않습니다
+		//이전과 동일하게 '국내 어디든 1인 1박에 10만원'이라는 슬로건을 유지할 사용자에게
+		//필요한 정보를 입력받아 예상 경비를 출력하도록 구현하세요 
+		
+		//여행사 프로모션2
+		
+		//입력
+		int people = 1;
+		int day = 1;
+		
+		Scanner sc = new Scanner(System.in);
+		
+	
+		sc.close();
+		
+		//계산
+		int price = 100000 * people * day;
+		
+		//출력
+		
+		
+		
+	}
+}

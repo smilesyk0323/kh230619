7ebae4fdafab2f1c5f4c4b68ff9ebae50f38f4c1
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 18:07:59 +0900

    주말 과제 생성

---
+++ day05/src/loop/Test04.java
@@ -0,0 +1,8 @@
+package loop;
+
+public class Test04 {
+	public static void main(String[]args) {
+		//1부터 99 사이에 5가 포함되어 있는 숫자만 필터링하여 출력 
+	}
+
+}
---
+++ day05/src/loop/Test05.java
@@ -0,0 +1,8 @@
+package loop;
+
+public class Test05 {
+	public static void main(String[]args) {
+		//1부터 99 사이에 369게임에서 박수 치는 숫자만 필터링 하여 출력
+	}
+
+}

7476497e3caf12f8324fa815ae4ed6317e3d5ee2
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 17:45:24 +0900

    반복문 예제

--- day05/src/loop/Test01.java
+++ day05/src/loop/Test01.java
@@ -2,6 +2,12 @@ package loop;
 
 public class Test01 {
 	public static void main(String[]args) {
-		
+		//반복 (loop)
+		// -(Q) Hello를 화면에 10번 출력하세요 
+		//for(int i=0; i <10; i++) {// i++  => i = i + 1
+		//for(int i=0; i <10; i +=1) {
+		for(int i =0 ; i < 10 ; i ++){
+			System.out.println("Hello");
+		}
 	}
 }

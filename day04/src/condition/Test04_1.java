af4228e3e32bc1ff7a89b2f222be229ca47219e4
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 09:25:14 +0900

    여행사 프로모션2 문제풀이

--- day04/src/condition/Test04_1.java
+++ day04/src/condition/Test04_1.java
@@ -13,20 +13,38 @@ public class Test04_1 {
 		//여행사 프로모션2
 		
 		//입력
-		int people = 1;
-		int day = 1;
-		
-		Scanner sc = new Scanner(System.in);
-		
-	
-		sc.close();
-		
-		//계산
-		int price = 100000 * people * day;
-		
-		//출력
-		
-		
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.print("인원수 : ");
+				int people = sc.nextInt();
+				
+				System.out.print("기간(일) : ");
+				int days = sc.nextInt();
+				
+				System.out.print("예정 월 : ");
+				int month = sc.nextInt();
+				
+				sc.close();
+				
+				//계산
+				int price = 100000 * people * days;
+				boolean summer = month == 6 || month == 7 || month == 8;
+				//int discount = 0 또는 25%금액;
+				int discount;
+				if(summer) {
+					discount = price * 25 / 100;
+				}
+				else {
+					discount = 0;
+				}
+				
+				int result = price - discount;
+				
+				
+				//출력
+				System.out.println("정상가 = " + price + "원");
+				System.out.println("할인금액 = " + discount +"원");
+				System.out.println("판매가 = " + result + "원");
 		
 	}
 }

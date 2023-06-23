7d21ba6f87b658af1369f9e4f20d38fbcb558151
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 13:58:08 +0900

    test05 문제 풀이

--- day05/src/condition2/Test05.java
+++ day05/src/condition2/Test05.java
@@ -2,6 +2,55 @@ package condition2;
 
 public class Test05 {
 	public static void main(String[]args) {
+		//SNS작성 시간 별 출력 문구 
+		//방금전 : 작성한 지 10초가 되지 않은 글 
+		//?초 전 : 작성한 지 10초 이상 1분 되지 않은 글 
+		//?분 전 : 작성한 지 1분 이상 1시간 미만인 글
+		//?시간 전 : 작성한 지 1시간 이상 1일 미만인 글
+		//?일 전 : 작성한 지 1일 이상인 글 
+		
+		// 사용자에게 '초'를 입력받아서 기준에 따라 게시글 시간 형식을 출력
+		
+		//int time = 100;
+		
+		//86400	< T 		?일 	전
+		//3600		<= T   	?시간 전
+		//60			<= T		?분 	전 
+		//10			<= T		?초 	전
+		//0			<= T   	방금	 전 
+		//int = result;
+		
+		//if(86400(/60/60/24)<time) {
+		//	result = time / 86400;
+		//}
+		//else if(3600(/60/60)<=time) {
+		//	result = time / 3600;
+		//}
+		//else if(60<= time) {
+		//	result = time / 60;
+		//}
+		//else if(10<= time) {
+		//	result = time / 10;
+		//}
+		//else {
+		//	result = time / 0;
+		//}
+		
+		
+		//초로 나온 단위를 각각 맞는 단위로 바꾸기 
+		// 
+		
+		//System.out.println(+ 환산시간 + 시간단위+"전")
+		//System.out println(result); 
+		
+		//풀이 
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 

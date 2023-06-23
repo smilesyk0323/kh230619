3bbd207c9f5bf29737f7a4cc96a7c10bcc06f659
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 17:52:54 +0900

    5회 입력받기 문제 풀이

---
+++ day05/src/loop/Test02.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test02 {
+	
+	public static void main(String[]args) {
+		//(주의) 도구 생성은 한번만 		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int  i=0; i<5; i++) {
+			System.out.println("숫자 입력: ");
+			int a = sc.nextInt();
+		}
+
+		sc.close();
+	
+	}	
+}

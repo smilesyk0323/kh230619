ad22ba255ff980d656dbab0152225ac33ce135fa
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 18:20:06 +0900

    주말 과제 풀이 중

--- day05/src/loop/Test04.java
+++ day05/src/loop/Test04.java
@@ -3,6 +3,12 @@ package loop;
 public class Test04 {
 	public static void main(String[]args) {
 		//1부터 99 사이에 5가 포함되어 있는 숫자만 필터링하여 출력 
+		
+		for(int i =1; i<100; i++) {
+			if(i/50==1 || (i-5)%10==0) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }

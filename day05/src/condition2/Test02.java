d3ad3f8914b68bfa4afb0e6595bd903b77b23d4a
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 11:00:43 +0900

    클래스명 변경

--- day05/src/condition2/Test02.java
+++ day05/src/condition2/Test02.java
@@ -1,6 +1,6 @@
 package condition2;
 
-public class Test2 {
+public class Test02 {
 	public static void main(String[]args) {
 		//그룹 조건 
 		//- 3가지 이상 중 하나의 경우를 선택하여 실행하는 상황 
@@ -14,18 +14,14 @@ public class Test2 {
 		if(hour <=7) {
 			System.out.println("버스 도착 시간을 알아봐줄게");
 		}
+		else if(hour <=8) {
+			System.out.println("지하철 시간을 알아봐줄게");
+		 }
+		else if(hour <= 9) {
+			System.out.println("택시를 불러줄게");
+		}
 		else {
-			if(hour <=8) {
-				System.out.println("지하철 시간을 알아봐줄게");
-			}
-			else {
-				if(hour <= 9) {
-					System.out.println("택시를 불러줄게");
-				}
-				else {
-					System.out.println("끝났어 그냥 천천히 가");
-				}
-			}
+			System.out.println("끝났어 그냥 천천히 가");
 		}
 	}
 }

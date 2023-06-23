4405a015e57e9fa0462d99a829e1db13939a0d26
smilesyk0323 <smilesyk0323@naver.com>
2023-06-23 16:26:51 +0900

    윤년 계산 풀이

--- day05/src/condition3/Test04.java
+++ day05/src/condition3/Test04.java
@@ -13,14 +13,14 @@ public class Test04 {
 
 		String result;
 		switch (month) {
-		case 1, 3, 5, 7, 8, 10, 12:
-			result = "큰달";
-		break;
-		case 4, 6, 9, 11:
-			result = "작은달";
-		break;
-		default:
-			
+			case 1, 3, 5, 7, 8, 10, 12:
+				result = "큰달";
+			break;
+			case 4, 6, 9, 11:
+				result = "작은달";
+			break;
+			default:
+				
 			result = "2월";
 			
 		}

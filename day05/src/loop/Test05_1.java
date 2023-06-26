70c74f15a09f73e44ab7016695e791607783af1f
smilesyk0323 <smilesyk0323@naver.com>
2023-06-26 09:59:20 +0900

    과제 풀이 심화

--- day05/src/loop/Test05_1.java
+++ day05/src/loop/Test05_1.java
@@ -1,24 +1,16 @@
 package loop;
 
-public class Test05 {
+public class Test05_1 {
 	public static void main(String[]args) {
 		//1부터 99 사이에 369게임에서 박수 치는 숫자만 필터링 하여 출력
 		//homework 03 풀이
-
 
 		for(int i=1; i <= 99; i++) {
 			boolean ten = i / 10 == 3 || i / 10 == 6 || i / 10 == 9;
 			boolean one = i % 10 == 3 || i % 10 == 6 || i % 10 == 9;
 			boolean clap = ten || one;
-			boolean doubleClap = ten && one;
 
-			if(doubleClap) {
-				System.out.println("짝짝");
-			}
-			else if(clap) {
-				System.out.println("짝");
-			}
-			else {
+			if(clap) {
 				System.out.println(i);
 			}
 		}

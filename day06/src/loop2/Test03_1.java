e5fffb9be030ae1a7449c1108f29d9e9a98b24d0
smilesyk0323 <smilesyk0323@naver.com>
2023-06-26 12:41:56 +0900

    369 박수치는 숫자 개수 예제 풀이

---
+++ day06/src/loop2/Test03_1.java
@@ -0,0 +1,19 @@
+package loop2;
+
+public class Test03_1 {
+	public static void main(String[]args) {
+		//1부터 99 사이의 숫자중 369 게임에서 박수치는 숫자의 개수를 구하여 출력 
+		//369박수치는 숫자 개수 구하기 	
+		int count =0; 
+		
+			for(int i = 1; i <=99; i ++){
+				boolean ten = i / 10 == 3 ||  i / 10 == 6 ||i / 10 == 9;
+				boolean one =  i % 10 == 3 || i % 10 == 6 || i % 10 == 9;
+	
+			if(ten ||one) {
+			   count ++;
+		    	}
+			}
+			System.out.println(count);
+	}
+}

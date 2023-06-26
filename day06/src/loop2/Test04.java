a24b057934a6a045320380bd808b39b1517440f3
smilesyk0323 <smilesyk0323@naver.com>
2023-06-26 12:42:24 +0900

    Test04 생성

---
+++ day06/src/loop2/Test04.java
@@ -0,0 +1,15 @@
+package loop2;
+
+import java.util.Scanner;
+
+public class Test04 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		
+		sc.close();
+		
+		System.out.println(a +"?!!");
+	}
+}

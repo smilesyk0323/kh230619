1fc120f35c5a21232072ddbee145d514484ae5f1
smilesyk0323 <smilesyk0323@naver.com>
2023-06-28 14:42:36 +0900

    백준 2884번 풀이 중

--- baekjoon/src/baekjoon/Test01.java
+++ baekjoon/src/baekjoon/Test01.java
@@ -4,26 +4,33 @@ import java.util.Scanner;
 
 public class Test01 {
     public static void main(String[]args){
-       
+
     	Scanner sc = new Scanner(System.in);
-    	    	
-    	int a = sc.nextInt();
-    	int b = sc.nextInt();
+    	 int H = sc.nextInt();
+    	 int M = sc.nextInt();
+
+    	 sc.close();
+    	 
+    	 int total;
+    	 
+    	 if(H>0) {
+    		 total = H * 60 + M;
+    		 System.out.println(total);
+    	 }
+    	 else {
+    		 total = M;
+    		 System.out.println(total);
+    		 if(total<60) {
+    			 
+    		 }
+    	 }
+    	  
+    	 
+    	 //int RT = total - 45;
+         
     	
-    	sc.close();
-    	
-    	if(a > b) {
-    		System.out.println(">");
-    	}
-    	else if(a<b) {
-    		System.out.println("<");
-    	}
-    	else if(a==b) {
-    		System.out.println("==");
-    	}
-    	else {
-    		
-    	}   	
+    	//System.out.println(RT/60);
+    	//System.out.println(RT%60);
     	
     }
 }

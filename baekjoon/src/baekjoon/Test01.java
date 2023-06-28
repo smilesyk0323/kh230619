5ddeaaff44321707e718f6605f9fd06938fa7233
smilesyk0323 <smilesyk0323@naver.com>
2023-06-28 09:37:02 +0900

    day08 생성

--- baekjoon/src/baekjoon/Test01.java
+++ baekjoon/src/baekjoon/Test01.java
@@ -1,20 +1,21 @@
 package baekjoon;
 
-	import java.util.Scanner;
+import java.util.Scanner;
 
-	public class Test01{
-	    public static void main(String[]args){
-	     
-	        Scanner sc = new Scanner(System.in);
-	        int a = sc.nextInt();
-	        int b = sc.nextInt();
-	        int c = sc.nextInt();
-	        sc.close();
-	        
-	        System.out.println((a+b)%c);
-	        System.out.println((a%c)+(b%c)%c);
-	        System.out.println((a*b)%c);
-	        System.out.println((a%c)*(b%c)%c);
-	        
-	    }
+public class Test01{
+    public static void main(String[]args){
+     
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); 
+        int b = sc.nextInt(); 
+        int c = sc.nextInt();
+
+        
+        System.out.println((a+b)%c);
+        System.out.println((a%c)+(b%c)%c);
+        System.out.println((a*b)%c);
+        System.out.println((a%c)*(b%c)%c);
+        
+        sc.close();
+    }
 }

788a170eb5ae8fd28274dc46f5bc55414077cb59
smilesyk0323 <smilesyk0323@naver.com>
2023-06-29 17:06:31 +0900

    회차별 주사위 정보 분석 문제 풀이

--- day09/src/array3/Test05_1.java
+++ day09/src/array3/Test05_1.java
@@ -4,36 +4,49 @@ import java.util.Random;
 
 public class Test05_1 {
 	public static void main(String[]args) {
-		//주사위를 5번씩 6회 던진 결과를 저장한 뒤 
-		//1.회차별 주사위 던진 결과를 출력 하시고 
-		//2.모든 주사위 값의 평균을 출력하세요 
-		
-		int[][] dice = new int[6][5];
-		
-		//for(int i = 0; i<dice.length;i++) {
-		//	for(int j= 0; j< dice[i].length; j++) {
-				
-		//	}
-		//}
 		Random r = new Random();
-		
-		//1번에 5번 던졌을때 
-		//int[] dice = new int[5];
-		for(int j = 0; j<dice.length; j++) {
-			System.out.print((j+1)+"번 ");
-			for(int i = 0; i<dice[j].length; i++) {
-					int result = r.nextInt(6)+1; 
-					System.out.print(result);
-				}
-			System.out.println("\t");
+
+		//배열 생성
+		int[][] diceList = new int[6][5];
+
+		//diceList	-->	diceList[0]		--> [ 0 ][ 0 ][ 0 ][ 0 ][ 0 ]	
+		//				-->	diceList[1]		--> [ 0 ][ 0 ][ 0 ][ 0 ][ 0 ]
+		//				-->	diceList[2]		--> [ 0 ][ 0 ][ 0 ][ 0 ][ 0 ]	
+		//				-->	diceList[3]		--> [ 0 ][ 0 ][ 0 ][ 0 ][ 0 ]	
+		//				-->	diceList[4]		--> [ 0 ][ 0 ][ 0 ][ 0 ][ 0 ]	
+		//				-->	diceList[5]		--> [ 0 ][ 0 ][ 0 ][ 0 ][ 0 ]	
+
+		for(int i=0; i < diceList.length; i++) {
+			for(int k=0; k< diceList[i].length; k++) {
+				diceList[i][k] = r.nextInt(6) + 1;
+			}
 		}
 
-	     for(int j = 0; j< dice.length; j++) {
-	    	 for(int i = 0; i<dice[j].length; i++) {
-	    		 //int result2 = r.nextInt(6)+1;
-	    	 }
+		//회차별 평균 계산
+		//= 합계 저장할 변수가 "회차" 개수만큼 있어야 한다
+		//int[] totalList = new int[6];
+		int[] totalList = new int[diceList.length];
+		float[] averageList = new float[diceList.length];
 
-	     }
+		for(int i=0; i < diceList.length; i++) {
+			for(int k=0; k < diceList[i].length; k++) {
+				totalList[i] += diceList[i][k];
+			}
+			averageList[i] = (float)totalList[i] / diceList[i].length;
+		}
+
+		for(int i=0; i < averageList.length; i++) {
+			System.out.println((i+1) + "회차 평균 = " + averageList[i]);
+		}
+
+		//출력
+		for(int i=0; i < diceList.length; i++) {
+			for(int k=0; k < diceList[i].length; k++) {
+				System.out.print(diceList[i][k]);
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+
 	}
-
 }

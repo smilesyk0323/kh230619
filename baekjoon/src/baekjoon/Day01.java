c4b7325907dfb078e69e42a670f8117f4bcf50e7
smilesyk0323 <smilesyk0323@naver.com>
2023-06-30 09:40:53 +0900

    10950번 풀이중

--- baekjoon/src/baekjoon/Day01.java
+++ baekjoon/src/baekjoon/Day01.java
@@ -6,15 +6,18 @@ public class Day01 {
 		public static void main(String[]args) {
 			
 			Scanner sc = new Scanner(System.in);
+			int T = sc.nextInt();
 			
-			int N = sc.nextInt();
-			
-			sc.close();
-			
-			for(int i =0; i<10; i++) {
+			for(int i =1; i <= T; i++) {
+				int A = sc.nextInt();
+				int B = sc.nextInt();
 				
+				int result = A + B;
+			    System.out.println( result);
 			}
-			
+						
+			sc.close();			
+						
 		}
 
 }

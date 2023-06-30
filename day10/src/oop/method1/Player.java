0134e8d4e4acdfc184794ea0b33a32d6202ec1d8
smilesyk0323 <smilesyk0323@naver.com>
2023-06-30 13:10:03 +0900

    메소드의 사용법

--- day10/src/oop/method1/Player.java
+++ day10/src/oop/method1/Player.java
@@ -1,9 +1,32 @@
 package oop.method1;
 //클래스
 public class Player {
-	// 멤버 필드 (멤버 변수)
+	// 멤버 필드 (멤버 변수) - 데이터
 	int rank;
 	String  name;
 	String  nation;
 	int  goal;
+	
+	//멤버 메소드 - 공통코드
+	//void 이름(){코드}
+	
+	//출력용
+	    void show() {
+		//this = 클래스의 멤버를 의미(내꺼)
+		System.out.print("순위: "+ this.rank+"\t");
+		System.out.print("이름: "+ this.name+"\t"	);
+		System.out.print("국가: "+ this.nation+"\t");
+		System.out.println("득점골: "+ this.goal);
+	    }
+		
+	//초기화용
+	//- 설정될 값을 알 수 없으므로 외부에서 조달받아서 초기화
+	//- 매개변수	
+		void setup(int rank, String name, String nation, int goal) {
+			this.rank = rank;
+			this.name = name;
+			this.nation = nation;
+			this.goal = goal;
+		}
+		
 }
--- day10/src/oop/method1/Test01.java
+++ day10/src/oop/method1/Test01.java
@@ -7,46 +7,17 @@ public class Test01 {
 			Player p3 = new Player();
 			Player p4 = new Player();
 			
-			p1.rank = 1;
-			p1.name = "칼리안 음바페";
-			p1.nation = "프랑스";
-			p1.goal = 8;
+			//초기화
+			p1.setup (1,"킬리안 음바페", "프랑스",8);
+			p2.setup( 2, "리오넬 메시","아르헨티나", 7);
+			p3.setup( 3,"올리비에 지루","프랑스", 4);
+			p4.setup(4, "풀리안 알바레스", "아르헨티나", 4);
 			
-			p2.rank = 2;
-			p2.name = "리오넬 메시";
-			p2.nation = "아르헨티나";
-			p2.goal = 7;
-			
-			p3.rank = 3;
-			p3.name = "올리비에 지루";
-			p3.nation = "프랑스";
-			p3.goal = 4;
-			
-			p4.rank = 4;
-			p4.name = "풀리안 알바레스";
-			p4.nation = "아르헨티나";
-			p4.goal = 4;
-			
-			System.out.print("순위: "+p1.rank+" 등"	+"\t");
-			System.out.print("이름: "+p1.name	+"\t");
-			System.out.print("국가: "+p1.nation +"\t");
-			System.out.println("득점골: "+p1.goal+" 개");
-			
-			System.out.print("순위: "+p2.rank+" 등"	+"\t");
-			System.out.print("이름: "+p2.name	+"\t"+"\t");
-			System.out.print("국가: "+p2.nation +"\t");
-			System.out.println("득점골: "+p2.goal+" 개");
-			
-			System.out.print("순위: "+p3.rank+" 등"	+"\t");
-			System.out.print("이름: "+p3.name	+"\t");
-			System.out.print("국가: "+p3.nation +"\t");
-			System.out.println("득점골: "+p3.goal+" 개");
-			
-			System.out.print("순위: "+p4.rank+" 등"	+"\t");
-			System.out.print("이름: "+p4.name	+"\t");
-			System.out.print("국가: "+p4.nation +"\t");
-			System.out.println("득점골: "+p4.goal+" 개");
-			
+			//출력
+			p1.show();//p1을 주인공으로 해서 show에 있는 코드를 실행해라
+			p2.show();
+			p3.show();
+			p4.show();
 			
 		}
 

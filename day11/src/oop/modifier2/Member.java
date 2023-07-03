6334f90e103486ee812f2b56fbb54af2a65f4319
smilesyk0323 <smilesyk0323@naver.com>
2023-07-03 20:25:53 +0900

    day11 과제 풀이

--- day11/src/oop/modifier2/Member.java
+++ day11/src/oop/modifier2/Member.java
@@ -1,5 +1,95 @@
 package oop.modifier2;
 
 public class Member {
-
+	
+	private String name;
+	private int year;
+	private int count;
+	private int money; 
+	private int account;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		switch(year) {
+		case 1:
+		case 2:
+		case 3:
+		case 5:
+		case 10:
+		}
+		this.year = year;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		
+		this.count = count;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		if(money<0) return;
+		this.money = money;
+	}
+	public int getAccount() {		
+		return account;
+	}
+	public void setAccount(int account) {
+		if(account<0) return;
+		this.account = account;
+	}
+	
+	public Member(String name,int year, int money) {
+		this(name,year,1,money,0);
+	}
+	public Member(String name, int year, int count, int money, int account) {
+		this.setName(name);
+		this.setYear(year);
+		this.setCount(count);
+		this.setMoney(money);
+		this.setAccount(account);
+	}
+	
+		
+	int getResult() {
+		return (this.year*12 + 1)*this.money;
+	}
+	
+	int getPayTotal() {
+		return this.count*this.money;
+	}
+	
+	int getCountYear() {
+		return (this.count-1) / 12;
+	}
+	
+	int getCountMonth() {
+		return (this.count-1) % 12;
+	}
+	
+	
+	public void show() {
+		System.out.println("<적급가입자정보>");
+		System.out.println("이름: "+this.name);
+		System.out.println("적금기간: "+this.year+"년");
+		System.out.println("총 납입기간: " + this.getCountYear()+"년"+this.getCountMonth()+"개월");
+		System.out.println("납입회차: "+this.count+"회차");
+		System.out.println("총납입금액: "+this.getPayTotal()+"만원");
+		System.out.println("현재총잔액: "+this.account+"만원");
+		if( this.getResult() <= account) {
+			System.out.println("만기");
+		}
+		System.out.println("만기예상금액: "+ this.getResult()+"만원");
+		System.out.println();		
+	}		
 }
--- day11/src/oop/modifier2/Test01.java
+++ day11/src/oop/modifier2/Test01.java
@@ -1,5 +1,14 @@
 package oop.modifier2;
 
 public class Test01 {
-
+	public static void main(String[]args) {
+		Member a = new Member("유재석",3,15,100,1500);
+		Member b = new Member("강호동",2,5,50,250);
+		Member c = new Member("신동엽",2,8,80,1000);
+		
+		a.show();
+		b.show();
+		c.show();
+		
+	}
 }

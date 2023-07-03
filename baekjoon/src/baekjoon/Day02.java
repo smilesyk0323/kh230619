e7e68fbf49a360139d2254173d0c543c85422ad0
smilesyk0323 <smilesyk0323@naver.com>
2023-07-03 09:34:06 +0900

    백준 25304번 풀이 중

---
+++ baekjoon/src/baekjoon/Day02.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Day02 {
+		public static void main(String[]args) {
+			Scanner sc = new Scanner(System.in);
+			
+			
+			int total = sc.nextInt();
+			
+			int countTotal = sc.nextInt();
+			
+			
+			int price = sc.nextInt();
+			int count = sc.nextInt();
+			
+		}
+
+}

67cfb027e5c888775bb714f3a1b63ae95e226c79
smilesyk0323 <smilesyk0323@naver.com>
2023-07-03 17:25:51 +0900

    private, public 접근 제한

---
+++ day11/src/oop/modifier1/Test01.java
@@ -0,0 +1,16 @@
+package oop.modifier1;
+
+public class Test01 {
+	public static void main(String[]args) {
+//		Student a = new Student();
+//		
+//		a.setName("피카츄");//a.name = "피카츄"; <- 에러남
+//		a.setScore(50);
+//		
+//		System.out.println(a.getName());
+//		System.out.println(a.getScore());
+		
+		Student b = new Student("피카츄",70);
+		b.show();
+	}
+}

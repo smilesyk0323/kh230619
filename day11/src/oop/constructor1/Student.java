6b0e4179e24a8e3cc2ac4ac8824103c9b38b3f2b
smilesyk0323 <smilesyk0323@naver.com>
2023-07-03 14:54:08 +0900

    생성자 예제

--- day11/src/oop/constructor1/Student.java
+++ day11/src/oop/constructor1/Student.java
@@ -1,5 +1,25 @@
 package oop.constructor1;
 
 public class Student {
+	String name;
+	int score;
+	
+	//기본 생성자
+	//- 생성만 하고 아무짓도 안함
+	//- 한 개도 없으면 자동생성됨
+	//- 생성자가 없으면 객체생성불가 
+	//Student(){}
+	
+	//- 생성자의 이름은 클래스 이름
+	//- 하는 일은 setup 메소드와 동일
+	//- 생성자도 오버로딩(overloading)이 가능
+	Student(String name){
+		this.name = name;
+		this.score = 0;
+	}
+	Student(String name, int score){
+		this.name = name;
+		this.score = score;
+	}
 
 }
--- day11/src/oop/constructor1/Test01.java
+++ day11/src/oop/constructor1/Test01.java
@@ -1,5 +1,11 @@
 package oop.constructor1;
 
 public class Test01 {
-
+	public static void main(String[]args) {
+		//Student a= new Student();
+		Student b = new Student("피카츄",50);
+		Student c = new Student("라이츄");
+		
+		//b.show();
+	}
 }

38c56454adaf0d8789cd3835e036ec02de4dcbb7
smilesyk0323 <smilesyk0323@naver.com>
2023-07-04 16:02:28 +0900

    상수의 생성 과정과 실제 사용

---
+++ day12/src/oop/keyword7/Test02.java
@@ -0,0 +1,8 @@
+package oop.keyword7;
+
+public class Test02 {
+	public static void main(String[]args) {
+		//System.out.println(Test01.scissors);//상수만 보관하기 위한 클래스도 있을 수 있다.
+		System.out.println(RSP.scissors);
+	}
+}

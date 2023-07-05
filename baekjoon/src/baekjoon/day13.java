4deeada8c60cc5e2df968b7f252bad7b48afa6a6
smilesyk0323 <smilesyk0323@naver.com>
2023-07-05 09:38:08 +0900

    문제풀이중

---
+++ baekjoon/src/baekjoon/day13.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class day13 {
+	public static void main(String[]args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		 int N = sc.nextInt();
+		 int count =0;
+		
+		for(int i = 0; i<N; i++) {
+			int a  = sc.nextInt();
+//			if(a == v) {
+//				count++;
+//			}
+		}
+		int v = sc.nextInt();
+		System.out.println(count);
+		
+	}
+}

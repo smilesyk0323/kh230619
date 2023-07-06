5906e9063fb1d261af4d2f82ae2fedca25a2fbc5
smilesyk0323 <smilesyk0323@naver.com>
2023-07-06 18:21:27 +0900

    과제 풀이

---
+++ day14/src/api/lang/string/Test04.java
@@ -0,0 +1,34 @@
+package api.lang.string;
+
+import java.util.Scanner;
+
+public class Test04 {
+		public static void main(String[]args) {
+			//사용할 닉네임 받도록 구현 , 다음과 같은 상황일 경우 오류 메세지 출력 
+			//- 닉네임이 2글자 이상 10글자 이하가 아닌경우 
+			//- 닉네임에 '운영자'라는 단어가 포함된 경우 
+			//모든 검사를 통과 했다면 '닉네임 설정이 완료 되었습니다'라는 메세지를 출력 
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("사용할 닉네임을 입력하세요: ");
+			String user = sc.next();
+			
+			int length1 = user.length();
+			
+			boolean ispass = length(user) >= length(2) && user <= length(10);
+			
+			if(닉네임이 2글자 이상 10글자 이하일 아닐 경우) {
+				System.out.println("닉네임의 글자수가 잘못 되었습니다");
+			}
+			else if(닉네임에 운영자가 포함될경우){
+				System.out.println("닉네임에 '운영자'를 포함할 수 없습니다");
+			}
+			else {
+				System.out.println("닉네임 설정이 완료 되었습니다");
+			}
+			
+			
+		}
+
+}

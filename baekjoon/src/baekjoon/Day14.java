bcb09420c0561856b76c191d8373de5d89240cc8
smilesyk0323 <smilesyk0323@naver.com>
2023-07-06 10:18:34 +0900

    문제풀이중

--- baekjoon/src/baekjoon/Day14.java
+++ baekjoon/src/baekjoon/Day14.java
@@ -6,7 +6,19 @@ public class Day14 {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int N = sc.nextInt();
+			
+		int [] a = new int[N];
 		
+		int X = sc.nextInt();
+		
+//		for(int i = 0; i < N) {
+//			int b = sc.nextInt();
+//		}
+//		
+//		if( a[i] < X) {
+////			
+//		}
 	}
 
 }

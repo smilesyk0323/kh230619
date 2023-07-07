a530432f205b6619df648bb5d93f5e7469abe90f
smilesyk0323 <smilesyk0323@naver.com>
2023-07-07 18:18:54 +0900

    회원가입프로그램

---
+++ day15/src/api/lang/string2/Member.java
@@ -0,0 +1,65 @@
+package api.lang.string2;
+
+import java.util.Scanner;
+
+public class Member {
+	private String id;
+	private String password;
+	private String name;
+	private String level; 
+	private int point;
+	
+	Member(){
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("아이디입력: ");
+		this.id = scanner.nextLine();
+		System.out.print("비밀번호입력: ");
+		this.password = scanner.nextLine();
+		System.out.print("회원이름: ");
+		this.name = scanner.nextLine();
+		System.out.print("회원레벨: ");
+		this.level = scanner.nextLine();
+		System.out.print("회원포인트: ");
+		this.point = scanner.nextInt();
+
+	}
+	
+	public String getId() {
+		return id;
+	}
+	String regex = "^[0-9a-z]{8,20}$";
+	public void setId(String id) {
+		if(id.matches(regex)) {
+			this.id = id;
+		}		
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	} 
+	
+
+
+}
---
+++ day15/src/api/lang/string2/Test07.java
@@ -0,0 +1,18 @@
+package api.lang.string2;
+
+public class Test07 {
+	public static void main(String[]args) {
+		Member member = new Member();
+		System.out.println("회원 아이디:"+ member.getId());
+		System.out.println("회원 비밀번호:"+ member.getPassword());
+		System.out.println("회원 이름:"+ member.getName());
+		System.out.println("회원 등급:"+ member.getLevel());
+		System.out.println("회원 포인트:"+ member.getPoint());
+		
+		
+		
+		
+	}
+	
+
+}

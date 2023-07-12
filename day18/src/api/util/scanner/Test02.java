cb8b59f350381ef6d5a7a4a23f7053b61fd8df70
smilesyk0323 <smilesyk0323@naver.com>
2023-07-12 10:26:01 +0900

    스캐너입력시 주의사항 수정

--- day18/src/api/util/scanner/Test02.java
+++ day18/src/api/util/scanner/Test02.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Test02 {
 	public static void main(String[]args) {
-		//결론 : next() 계열의 명령 뒤에 .nextLIne()을 쓸 경우 비어있는 명령 한개
+		//결론 : next() 계열의 명령 뒤에 .nextLIne()을 쓸 경우 비어있는 명령 한개를 추가 
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("입력 :");
@@ -17,7 +17,8 @@ public class Test02 {
 		sc.close();
 		
 		System.out.println(" a= " + a);
-		System.out.println(" b= " + b);
+		System.out.println(" b= " + b
+				);
 		
 		
 }

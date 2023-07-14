5c48acd305ce9278b2d1ec3c89b45f641c91c752
smilesyk0323 <smilesyk0323@naver.com>
2023-07-14 17:16:38 +0900

    파일복사 프로그램(1byte단위)

---
+++ day20/src/api/file/single/FileUtil.java
@@ -0,0 +1,10 @@
+package api.file.single;
+
+public class FileUtil {
+	//파일 복사 메소드 
+	
+//	public static void copy(File readTarget, File wirteTarget) {
+//		
+//	}
+
+}
---
+++ day20/src/api/file/single/Test02_1.java
@@ -0,0 +1,28 @@
+package api.file.single;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Test02_1 {
+	public static void main(String[] args) throws IOException {
+		//Test01에서 저장한 파일 내용을 불러오도록 구현 
+		
+		//준비물  
+		//1.대상 파일 객체 
+		//2.입력용 스트림 
+		
+		File target = new File("sample", "single.kh");
+		FileInputStream stream = new FileInputStream(target);
+		
+		// [프로그램]<--[입력통로(stream)]<--[파일객체(target)]<--[실제파일]
+		
+		while(true) {
+			int a =stream.read();
+			if(a==-1) break;//파일의 끝 - End Of File = EOF 처리
+			System.out.println("a = "+ a);
+		}
+		stream.close();
+		
+}
+}

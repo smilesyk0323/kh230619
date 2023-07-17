588f72aaa9ad8868542118169de73f6f0b81a59d
smilesyk0323 <smilesyk0323@naver.com>
2023-07-17 09:54:12 +0900

    백준문제 풀이

---
+++ baekjoon/src/baekjoon/Day21.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Day21 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+			
+		
+		int X = sc.nextInt();
+		
+		for(int i = 0; i < N;i++) {
+			int b = sc.nextInt();
+		}
+		int [] a = new int[N];
+		
+		
+
+		
+	}
+
+}
+

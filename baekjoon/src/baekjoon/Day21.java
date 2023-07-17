3535213671906b78d4b0faa29592ac0e95f7bfd5
smilesyk0323 <smilesyk0323@naver.com>
2023-07-17 15:52:26 +0900

    내가 만든 클래스 객체 입출력

--- baekjoon/src/baekjoon/Day21.java
+++ baekjoon/src/baekjoon/Day21.java
@@ -8,19 +8,16 @@ public class Day21 {
 		
 		int N = sc.nextInt();
 				
-		int X = sc.nextInt();
-		int[] list = new int[N];
+		int[]list = new int[N];
 		
-		for(int i = 0; i < N;i++) {
+		for(int i = 0; i < N; i++) {
 			list[i] = sc.nextInt();
 		}
 		
-		for(int i =0; i<list.length; i++	) {
-			if(list[i] < X) {
-				System.out.println(list[i]);
-			}
+		for(int i = 0; i < list.length; i++) {
+			
 		}
-
+	
 		
 		
 

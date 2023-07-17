c70c05e54026128c0a5e54d1b1a3300a290e65b7
smilesyk0323 <smilesyk0323@naver.com>
2023-07-17 10:27:00 +0900

    day20예제풀이

--- day20/src/api/file/single2/Tet03.java
+++ day20/src/api/file/single2/Tet03.java
@@ -10,11 +10,12 @@ public class Tet03 {
 		//파일복사 프로그램 (바이트 배열 - 버퍼 사용)
 		
 		//배열 준비 
-		byte[] buffer = new byte[10];
+		byte[] buffer = new byte[8192];
 		
-		//대상 준비 
-		File readTarget = new File("경로");
-		File writeTarget = new File("경로");
+		//대상 준비 ([1]운영체제가 거부하는 위치인 경우)
+		//String home = System.getproperty("user.home");//[1]
+		File readTarget = new File("D:/origin.txt");//home,나머지경로
+		File writeTarget = new File("sample/copy3.txt");//home,나머지경로 
 		
 		//스트림 준비 
 		FileInputStream readStream = new FileInputStream(readTarget);
@@ -22,11 +23,17 @@ public class Tet03 {
 		
 		//[읽을파일] --> writeTarget --> readStream ->[프로그램]-> writeStram -> writeTarget-->[내보낼파일]
 		
+		long start = System.currentTimeMillis();
+		
 		while(true) {
 			int size = readStream.read(buffer);
 			if(size == -1) break;
 			writeStream.write(buffer, 0 , size);			
 		}
+		
+		long finish = System.currentTimeMillis();
+		long duration = finish - start;
+		System.out.println("소요시간 = " + duration + "ms");
 		
 		//파일 통로 정리 
 		readStream.close();

96a7a855d54685f9cffb131b2842c28356bab19a
smilesyk0323 <smilesyk0323@naver.com>
2023-07-18 10:02:12 +0900

    문자열 출력

--- day22/src/api/file/string/Test01.java
+++ day22/src/api/file/string/Test01.java
@@ -1,5 +1,29 @@
 package api.file.string;
 
-public class Test01 {
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
+public class Test01 {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		//문자열 파일 입출력
+		//= 문자열은 byte 또는 char와 상호변환이 가능 
+		
+		String s1 = "Hello 안녕하세요";
+		
+		byte[] b1 = s1.getBytes();
+		System.out.println(Arrays.toString(b1));//음수가 보이면 유니코드 
+		
+		byte[] b2 = s1.getBytes("MS949");
+		byte[] b3 = s1.getBytes("UTF-8");
+		
+		System.out.println(Arrays.toString(b2));
+		System.out.println(Arrays.toString(b3));
+		
+		String s2 = new String(b2,"MS949");
+		String s3 = new String(b3/*,"UTF-8"*/);
+		System.out.println("s2 = "+ s2);//유니코드는 깨짐
+		System.out.println("s3 = "+ s3);//아스키 코드느 안깨짐
+		
+		
+	}
 }
---
+++ day22/src/api/file/string/Test02.java
@@ -0,0 +1,29 @@
+package api.file.string;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Test02 {
+	public static void main(String[] args) throws IOException {
+		//문자열 출력
+		//= 문자열 출력은 필연적으로 인코딩이 필요 
+		//= Writer 계열의 클래스를 사용하여 처리 
+		
+		File directory = new File("sample");
+		directory.mkdirs();
+		File target = new File(directory,"string.kh");
+//		FileOutputStream stream = new FileOutputStream(target);
+		FileWriter fw = new FileWriter(target);//자체 버퍼가 있지만 성능이 좋지 않음 
+//		BufferedOutputStream buffer = new BufferedOutputStream(stream);
+		BufferedWriter bw = new BufferedWriter(fw);//버퍼의 크기 8192
+		
+		bw.write("Hello");
+		bw.write("안녕하세요");
+		
+		//정리 
+//		bw.flush();//앞으로 사용 계획 O
+		bw.close();//앞으로 사용 계획 X
+	}
+}

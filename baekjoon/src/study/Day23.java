7369ed3c6a3c3d477da6a974efdabce50b505913
smilesyk0323 <smilesyk0323@naver.com>
2023-07-21 10:28:41 +0900

    자습

---
+++ baekjoon/src/study/Day23.java
@@ -0,0 +1,17 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Day23 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer>h = new HashSet<>();
+		
+		for(int i = 0; i<10; i++) {
+			h.add(sc.nextInt()%42);
+		}
+			sc.close();
+			System.out.println(h.size());
+	}
+}

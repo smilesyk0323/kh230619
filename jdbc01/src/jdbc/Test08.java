12d2d2452758b4e319793fd89b4590bf510f0a31
smilesyk0323 <smilesyk0323@naver.com>
2023-07-21 16:29:33 +0900

    추가 문제풀이 수정

--- jdbc01/src/jdbc/Test08.java
+++ jdbc01/src/jdbc/Test08.java
@@ -2,19 +2,31 @@ package jdbc;
 
 import java.util.Scanner;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 public class Test08 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("회원아이디: ");
 		String member_id = sc.nextLine();
+		System.out.print("회원비밀번호: ");
 		String member_pw = sc.nextLine();
+		System.out.print("회원닉네임: ");
 		String member_nickname = sc.nextLine();
+		System.out.print("회원생년월일: ");
 		String member_birth = sc.nextLine();
+		System.out.print("회원메일주소: ");
 		String member_email = sc.nextLine();
+		System.out.print("회원연락처: ");
 		String member_contact = sc.nextLine();
+		System.out.print("회원등급: ");
 		String member_level = sc.nextLine();
+		System.out.print("회원포인트: ");
 		String member_point = sc.nextLine();
+		System.out.print("회원가입일: ");
 		String member_join = sc.nextLine();
 		
 		sc.close();
@@ -24,7 +36,19 @@ public class Test08 {
 				+ "member_point, member_join) "
 				+ "values(?,?,?,?,?,?,?,?,?)";
 		
-		Object[]data = {};
+		Object[]data = {member_id, member_pw, member_nickname, member_birth, member_email, member_contact, 
+								member_level, member_point, member_join};
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		dataSource.setUsername("C##KH");
+		dataSource.setPassword("KH");
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.update(sql,data);
+		
+		System.out.println("회원정보등록!");
 		
 	}
 

a8d1c100b09b73385501f60c4874da9107d96e9e
smilesyk0323 <smilesyk0323@naver.com>
2023-07-24 13:50:04 +0900

    게시물 키워드 검색 추가 수정

--- jdbc02/src/jdbc/select/Test04.java
+++ jdbc02/src/jdbc/select/Test04.java
@@ -11,11 +11,17 @@ import jdbc.util.JdbcUtils;
 public class Test04 {
 	public static void main(String[] args) {
 		
-		String sql = "select * from board order by board_no desc";
+		//Board 검색
+		String keyword = "테스트";
+		
+		String sql = "select * from board "
+				+ "where instr(board_title,?)>0"
+				+ "order by board_no desc";
+		Object[] data = {keyword};
 		
 		BoardMapper mapper = new BoardMapper();
 		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
-		List<BoardDto>list = jdbcTemplate.query(sql, mapper);
+		List<BoardDto>list = jdbcTemplate.query(sql, mapper,data);
 		
 //		if(list.size() == 0) {//값이 비어 있을경우 
 		if(list.isEmpty()) {	

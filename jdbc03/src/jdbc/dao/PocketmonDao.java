216c4530e988255225b33c39baa51c71ae7518a1
smilesyk0323 <smilesyk0323@naver.com>
2023-07-25 10:10:49 +0900

    포켓몬 정보 수정

--- jdbc03/src/jdbc/dao/PocketmonDao.java
+++ jdbc03/src/jdbc/dao/PocketmonDao.java
@@ -2,6 +2,7 @@ package jdbc.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import jdbc.dto.PocketmonDto;
 import jdbc.util.JdbcUtils;
 
 //DAO클래스 
@@ -10,13 +11,62 @@ import jdbc.util.JdbcUtils;
 //= 기본적으로 C(등록), R(목록), R(상세), U(수정), D(삭제)가 필요 
 public class PocketmonDao {
 	
-	//C(등록) 메소드 
-	public void insert(int no, String name, String type) {
+		//C(등록) 메소드 
+		public void insert(int no, String name, String type) {
 
 		String sql = "insert into pocketmon(no, name, type) "
 						+ "values(?,?,?)";
 		Object[] data = {no, name, type};
 		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
-		jdbcTemplate.update(sql,data);
+		jdbcTemplate.update(sql,data);	
+	 }
+	
+		//바람직한 형태의 등록 메소드
+		public void insert(PocketmonDto dto) {
+			String sql = "insert into pocketmon(no, name, type) "
+					+ "values(?, ?, ?)";
+			Object[] data = {dto.getNo(), dto.getName(), dto.getType()};
+			
+			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+			jdbcTemplate.update(sql, data);
+		}
+		
+		//U(수정) 메소드 
+		//- no를 이용해서 name과 type을 바꾼다 
+		//pocketmonDto=포켓몬 한마리 정보 
+		//- 적용된 행이 있는지 없는지를 알아야 한다
+		public boolean update(PocketmonDto dto){//꼭확인을 위해서 boolean사용
+			String sql = "update pocketmon set name =?, type=? where no=?";
+			Object[] data = {
+					dto.getName(), dto.getType(), dto.getNo()
+			};
+			
+			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+			int result = jdbcTemplate.update(sql,data);
+			
+			//if(result >0) return true;//result가 0보다 크면 참 아니면 반환해라
+			//else return false;
+			
+			return result > 0 ;//result가 0보다 큰지 판정해서 반환해라 
+		
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

6d14be41c52306af46b8a249be9dd158a80d3469
smilesyk0323 <smilesyk0323@naver.com>
2023-07-26 09:55:02 +0900

    회원정보 상세 조회 풀이 수정

--- jdbc03/src/jdbc/app/Test22.java
+++ jdbc03/src/jdbc/app/Test22.java
@@ -5,10 +5,10 @@ import jdbc.dto.MemberDto;
 
 public class Test22 {
 	public static void main(String[] args) {
-		String MemberId = "testuser0";
+		String memberId = "testuser0";
 		
 		MemberDao dao = new MemberDao();
-		MemberDto dto = dao.selectOne(MemberId);
+		MemberDto dto = dao.selectOne(memberId);
 		
 		if(dto==null) {
 			System.out.println("찾으시는 회원 정보가 없습니다");

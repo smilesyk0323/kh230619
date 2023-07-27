2ada1cdb6b342f5e780eebf48c400cbd88c91f2f
smilesyk0323 <smilesyk0323@naver.com>
2023-07-27 14:46:36 +0900

    게시글 등록 컨트롤러 DTO+DAO

---
+++ spring06board/src/main/java/com/kh/spring06/dao/BoardDao.java
@@ -0,0 +1,23 @@
+package com.kh.spring06.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring06.dto.BoardDto;
+
+@Repository
+public class BoardDao {
+		@Autowired
+		private JdbcTemplate jdbcTemplate;
+		
+		public void insert(BoardDto dto) {
+			String sql = "insert into board "
+					     + "(board_no,board_title, board_content, board_writer, "
+					     + "board_readcount)"
+							+ "values(board_seq.nextval,?,?,?,0)";
+			Object[] data = { dto.getBoardTitle(), dto.getBoardContent(), 
+									dto.getBoardWriter()};
+			jdbcTemplate.update(sql,data);
+	}
+}

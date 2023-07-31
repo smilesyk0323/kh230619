43a99e089fe2c9b37c263d051f1c5a3af8b28897
smilesyk0323 <smilesyk0323@naver.com>
2023-07-31 09:56:17 +0900

    문제풀이

--- baekjoon/src/baekjoon/Day22.java
+++ baekjoon/src/baekjoon/Day22.java
@@ -2,6 +2,12 @@ package baekjoon;
 
 public class Day22 {
 		public static void main(String[] args) {
-
+			System.out.println("         ,r'\"7");
+			System.out.println("r`-_   ,'  ,/");
+			System.out.println(" \\. \". L_r'");
+			System.out.println("   `~\\/");
+			System.out.println("      |");
+			System.out.println("      |");
+			
 	}
 }

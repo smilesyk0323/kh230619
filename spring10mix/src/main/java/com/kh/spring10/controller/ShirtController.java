f32aa05c82a1eed219745f05569217f7543e4da5
bora <bora@DESKTOP-2N14FI1>
2023-08-06 00:43:32 +0900

    shirt dao, controller 작성

--- spring10mix/src/main/java/com/kh/spring10/controller/ShirtController.java
+++ spring10mix/src/main/java/com/kh/spring10/controller/ShirtController.java
@@ -1,5 +1,105 @@
 package com.kh.spring10.controller;
 
-public class ShirtController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kh.spring10.dao.ShirtDao;
+import com.kh.spring10.dto.ShirtDto;
+
+@Controller
+@RequestMapping("/shirt")
+public class ShirtController {
+	
+		@Autowired
+		private ShirtDao dao;
+		
+		@RequestMapping("/detail")
+			public String detail(@RequestParam int shirtNo, Model model	) {
+			ShirtDto dto = dao.selectOne(shirtNo);
+			model.addAttribute("dto", dto);
+			return "/WEB-INF/views/shirt/detail.jsp";
+		}
+
+		@RequestMapping("/list")
+			public String list(Model model) {
+				List<ShirtDto>list = dao.selectList();
+				model.addAttribute("list", list);
+				return "/WEB-INF/views/shirt/list.jsp";
+		}
+		
+		@GetMapping("/add")
+			public String insert() {
+				return "/WEB-INF/views/shirt/add.jsp";
+		}
+		
+		@PostMapping("/add")
+			public String insert(@ModelAttribute ShirtDto dto) {
+				int shirtNo = dao.sequence();
+				dto.setShirtNo(shirtNo);
+				dao.insert(dto);
+				return "redirect:detail?shirtNo="+shirtNo;
+		}
+		
+		@GetMapping("/edit")
+		public String update(@RequestParam int shirtNo, Model model) {
+			ShirtDto dto = dao.selectOne(shirtNo);
+			model.addAttribute("dto", dto);
+			return "/WEB-INF/views/shirt/edit.jsp";
+		}
+		
+		@PostMapping("/edit")
+		public String update(@ModelAttribute ShirtDto dto) {
+				boolean result = dao.update(dto);
+				if(result) {
+					return "redirect:detail?shirtNo="+dto.getShirtNo();
+				}
+				else {
+					return "redirect:에러페이지주소";
+				}
+		}
+		
+		@RequestMapping("/delete")
+			public String delete(@RequestParam int shirtNo) {
+				boolean result = dao.delete(shirtNo);
+				if(result) {
+					return "redirect:list";
+				}
+				else {
+					return "redirect:오류페이지";
+				}
+		}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spring10mix/src/main/webapp/WEB-INF/views/shirt/add.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>상품 등록</title>
+</head>
+<body>
+	<h1>상품등록</h1>
+		<form action="add" method="post">
+			<input type="text" name="shirtName" required><br><br>
+			<input><br><br>
+			<input><br><br>
+			<input><br><br>
+			<input><br><br>
+			<input><br><br>
+			<input><br><br>
+		</form>
+</body>
+</html>

da117bd19a9ca19d0c3305d24b38d2566d0ec3f9
smilesyk0323 <smilesyk0323@naver.com>
2023-09-13 17:56:53 +0900

    포켓몬스터 이미지와 연결 처리

--- springhome/src/main/java/com/kh/springhome/controller/PocketmonController.java
+++ springhome/src/main/java/com/kh/springhome/controller/PocketmonController.java
@@ -1,19 +1,80 @@
 package com.kh.springhome.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.springhome.dao.AttachDao;
+import com.kh.springhome.dao.PocketmonDao;
+import com.kh.springhome.dto.AttachDto;
+import com.kh.springhome.dto.PocketmonDto;
 
 @Controller
 @RequestMapping("/pocketmon")
 public class PocketmonController {
 	
-	@GetMapping("/insert")
-	public String insert(){
-		return "/WEB-INF/views/pocketmon/insert.jsp";
-	}
+	@Autowired
+	private PocketmonDao pocketmonDao;
 	
-//	@PostMapping("/insert")
+	@Autowired
+	private AttachDao attachDao;
+
+	@GetMapping("/insert")
+		public String insert(){
+			return "/WEB-INF/views/pocketmon/insert.jsp";
+		}
+		
+	
+	@PostMapping("/insert")
+	public String insert(@ModelAttribute PocketmonDto pocketmonDto, 
+									@RequestParam MultipartFile attach) throws IllegalStateException, IOException {
+		//포켓몬 등록
+		int no = pocketmonDao.sequence();
+		pocketmonDto.setNo(no);
+		pocketmonDao.insert(pocketmonDto);
+		
+		if(!attach.isEmpty()) {
+			//첨부파일등록(파일이 있을때만)
+			int attachNo = attachDao.sequence();
+			
+			String home = System.getProperty("user.home");
+			File dir = new File(home, "upload");
+			dir.mkdir();
+			File target = new File(dir, String.valueOf(attachNo));
+			attach.transferTo(target);
+			
+			AttachDto attachDto = new AttachDto();
+			attachDto.setAttachNo(attachNo);
+			attachDto.setAttachName(attach.getOriginalFilename());
+			attachDto.setAttachSize(attach.getSize());
+			attachDto.setAttachType(attach.getContentType());
+			attachDao.insert(attachDto);
+			
+			//연결(파일이 있을때만)
+			pocketmonDao.connect(no, attachNo);
+		}
+		
+		return "redirect:insert";
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- springhome/src/main/java/com/kh/springhome/dao/PocketmonDaoImpl.java
+++ springhome/src/main/java/com/kh/springhome/dao/PocketmonDaoImpl.java
@@ -33,4 +33,23 @@ public class PocketmonDaoImpl implements PocketmonDao{
 		jdbcTemplate.update(sql,data);
 	}
 
+	@Override
+	public void connect(int pocketmonNo, int attachNo) {
+		String sql = "insert into pocketmon_image values(?,?)";
+		Object[] data = {pocketmonNo, attachNo};
+		jdbcTemplate.update(sql,data);
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+

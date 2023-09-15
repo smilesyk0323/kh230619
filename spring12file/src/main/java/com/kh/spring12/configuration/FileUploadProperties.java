97ee387179a38779d768af59781f6c7e58acc9c0
smilesyk0323 <smilesyk0323@naver.com>
2023-09-15 10:53:01 +0900

    properties 설정

---
+++ spring12file/src/main/java/com/kh/spring12/configuration/FileUploadProperties.java
@@ -0,0 +1,36 @@
+package com.kh.spring12.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+//application.properties에 등록한 커스텀 속성 중 
+//custom.fileupload로 시작하는 설정을 불러오는 파일
+@Data//setter메소드 필요
+@Component
+@ConfigurationProperties(prefix="custom.fileupload")
+public class FileUploadProperties {
+	private String home;	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
+++ spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.spring12.configuration.FileUploadProperties;
 import com.kh.spring12.dao.AttachDao;
 import com.kh.spring12.dto.AttachDto;
 
@@ -29,6 +32,20 @@ public class FileRestController {
 
 		@Autowired
 		private AttachDao attachDao;
+		
+		//미리 작성해둔 커스텀 속성을 불러와서 디렉터리 객체까지 생성 
+		@Autowired
+		private FileUploadProperties props;
+		
+		private File dir;
+		
+		//처음 한번만 만들어야하는 세팅
+		//내가 만든건  @Autowired 안함
+		@PostConstruct//모든 로딩이 끝나면 자동으로 실행되는 메소드 
+		public void init() {
+			dir = new File(props.getHome());
+			dir.mkdirs();
+		}
 		
 		//비동기 통신에서는 화면에서 다음 작업이 가능하도록 파일 번호 등을
 		@PostMapping("/upload")//무조건 업로드는 포스트매핑
@@ -42,12 +59,13 @@ public class FileRestController {
 			//[2] 시퀀스 번호를 파일명으로 하여 실제 파일을 저장한다
 			//- 같은 이름에 대한 충돌을 방지하기 위해 
 			
-			String home = System.getProperty("user.home");
-			File dir = new File(home,"upload");
-			dir.mkdirs();//디렉터리 생성
+			//내가 만든 경로 세팅으로 주석처리 
+//			String home = System.getProperty("user.home");
+//			File dir = new File(home,"upload");
+//			dir.mkdirs();//디렉터리 생성
 			
 			//저장할 파일(attach.getOriginalFilename - 중복가능성이있음)
-			File target = new File(dir, String.valueOf(attachNo));
+			File target = new File(dir, String.valueOf(attachNo));//설정파일에 작성되어있는 경로
 			attach.transferTo(target);//저장
 			
 			//[3] DB에 저장한 파일의 정보를 모아서 INSERT
@@ -72,25 +90,24 @@ public class FileRestController {
 			if(attachDto == null) {
 				return ResponseEntity.notFound().build();//404 반환
 			}	
-			String home = System.getProperty("user.home");
-			File dir = new File(home, "upload");
+			//내가 만든 경로 세팅으로 주석처리 
+//			String home = System.getProperty("user.home");
+//			File dir = new File(home, "upload");
 			File Target = new File(dir, String.valueOf(attachDto.getAttachNo()));
 			
 			byte[] data = FileUtils.readFileToByteArray(Target);//실제 파일정보 불러오기
 			ByteArrayResource resource =new ByteArrayResource(data);
 			
-			 return ResponseEntity.ok()
-
-						.header(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name())	 
-
-						.contentLength(attachDto.getAttachSize())
-
-						.header(HttpHeaders.CONTENT_TYPE, attachDto.getAttachType())
-						.header(HttpHeaders.CONTENT_DISPOSITION, 
-								ContentDisposition.attachment()
-									.filename(attachDto.getAttachName(),StandardCharsets.UTF_8)//파일명, 인코딩
-									.build().toString())	
-						.body(resource);//파일정보
+			return ResponseEntity.ok()
+					.header(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name())
+					.contentLength(attachDto.getAttachSize())
+					.header(HttpHeaders.CONTENT_TYPE, attachDto.getAttachType())
+					.header(HttpHeaders.CONTENT_DISPOSITION, 
+						ContentDisposition.attachment()
+						.filename(attachDto.getAttachName(), StandardCharsets.UTF_8)
+						.build().toString()
+					)
+				.body(resource);
 		}
 			
 			

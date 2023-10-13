d5d158c8060a7ee65825b41b8b7570157de1e076
smilesyk0323 <smilesyk0323@naver.com>
2023-10-13 10:10:57 +0900

    복합검색 번호 구현

---
+++ spring15book/src/test/java/com/kh/spring15/Test07.java
@@ -0,0 +1,34 @@
+package com.kh.spring15;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring15.dto.BookDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class Test07 {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() {
+		Map<String, Object> param = new HashMap<>();
+		param.put("bookId",3);
+		
+		List<BookDto> list = sqlSession.selectList("book.complexSearch", param);
+		log.debug("검색 결과 = {}개", list.size());
+		for(BookDto dto : list) {
+			log.debug("dto = {}", dto);
+		}
+	}
+}

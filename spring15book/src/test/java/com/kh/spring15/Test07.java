50fbf98513022c7cc3e723107ccb146771424069
smilesyk0323 <smilesyk0323@naver.com>
2023-10-13 13:16:26 +0900

    복합검색 페이지네이션

--- spring15book/src/test/java/com/kh/spring15/Test07.java
+++ spring15book/src/test/java/com/kh/spring15/Test07.java
@@ -31,8 +31,17 @@ public class Test07 {
 //		param.put("bookPriceMax", 1000);
 //		param.put("bookPublicationBegin", "2023-01-01");
 //		param.put("bookPublicationEnd", "2023-12-31");
-		param.put("bookPageMin", 100);
-		param.put("bookPageMax", 500);
+//		param.put("bookPageMin", 100);
+//		param.put("bookPageMax", 500);
+//		param.put("bookGenre", "판타지");//장르가 한개였다면...(X)
+//		param.put("bookGenreList", List.of("판타지","호러","로맨스"));//장르가 여러개라면..(O)
+		
+//		param.put("orderList", List.of("book_id asc"));//번호순
+//		param.put("orderList", List.of("book_publication_date desc", "book_id asc"));//최근 출간순(같)
+		
+//		param.put("begin", 1);
+//		param.put("end", 10);
+
 		
 		List<BookDto> list = sqlSession.selectList("book.complexSearch", param);
 		log.debug("검색 결과 = {}개", list.size());

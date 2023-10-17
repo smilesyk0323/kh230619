9488c441c9dc6143c6d1d7a9bd0b22e2d56b970f
smilesyk0323 <smilesyk0323@naver.com>
2023-10-17 10:47:03 +0900

    회원가입 화면 구현

---
+++ spring18security/src/main/java/com/kh/spring18/controller/SecureMemberController.java
@@ -0,0 +1,35 @@
+package com.kh.spring18.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring18.dao.SecureMemberDao;
+import com.kh.spring18.dto.SecureMemberDto;
+
+@Controller
+@RequestMapping("/secure")
+public class SecureMemberController {
+
+	@Autowired
+	private SecureMemberDao dao;
+	
+	@GetMapping("/join")
+	public String join() {
+		return "/WEB-INF/views/secure/join.jsp";
+	}
+	
+	@PostMapping("/join")
+	public String join(@ModelAttribute SecureMemberDto dto) {
+		dao.insert(dto);
+		return "redirect:joinFinish";
+	}
+	@GetMapping("/joinFinish")
+	public String joinFinish() {
+		return "/WEB-INF/views/secure/join.jsp";
+	}
+	
+}

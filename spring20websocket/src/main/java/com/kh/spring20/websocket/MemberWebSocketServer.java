45f262c86d30ad35a87455904f93ff1ee4a51964
smilesyk0323 <smilesyk0323@naver.com>
2023-10-20 15:56:20 +0900

    비회원은 채팅을 전달하지 않도록 처리

--- spring20websocket/src/main/java/com/kh/spring20/websocket/MemberWebSocketServer.java
+++ spring20websocket/src/main/java/com/kh/spring20/websocket/MemberWebSocketServer.java
@@ -1,12 +1,12 @@
 package com.kh.spring20.websocket;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -21,6 +21,14 @@ public class MemberWebSocketServer extends TextWebSocketHandler{
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		clients.add(session);
+		log.debug("session = {}", session.getAttributes());
+		
+		//session의 추가 정보(attributes)를 조사하여 HttpSession의 정보를 추출하여 사용 
+		Map<String, Object> attr = session.getAttributes();
+		String memberId = (String) attr.get("name");
+		String memberLevel = (String) attr.get("level");
+		log.debug("아이디 = {}, 등급 = {}", memberId, memberLevel);
+		
 		log.debug("사용자 접속 = {}", clients.size());				
 	}
 	
@@ -32,6 +40,14 @@ public class MemberWebSocketServer extends TextWebSocketHandler{
 	
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		//기존 HTTP 세션의 정보를 조회	
+		Map<String, Object> attr = session.getAttributes();
+		String memberId = (String) attr.get("name");
+		String memberLevel = (String) attr.get("level");
+		if(memberId == null || memberLevel == null) {//비회원이라면
+			return;
+		}
+		
 		for(WebSocketSession client : clients) {
 			client.sendMessage(message);
 		}
--- spring20websocket/src/main/webapp/WEB-INF/views/home.jsp
+++ spring20websocket/src/main/webapp/WEB-INF/views/home.jsp
@@ -8,6 +8,22 @@
 <h2><a href="time">타임 웹소켓 예제</a></h2>
 <h2><a href="group">그룹 웹소켓 예제</a></h2>
 
+<c:choose>
+	<c:when test="${sessionScope.name == null}">
+		<!-- 로그인 화면 -->
+		<form action="login"method="post">
+			ID<input type="text"name="memberId">
+			<br><br>
+			PW<input type="password"name="memberPw">
+			<button type="submit">로그인</button>
+		</form>
+	</c:when>
+	<c:otherwise>
+		<a href="logout">로그아웃</a>
+	</c:otherwise>
+</c:choose>
+
+<h2><a href="member">회원 전용 웹소켓 예제</a></h2>
 
 
 

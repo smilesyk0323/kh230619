ef3918ae0a892e6d70dcb59923740d0e573d17fb
smilesyk0323 <smilesyk0323@naver.com>
2023-10-24 16:06:32 +0900

    kakaopay 승인 테스트

--- spring21pay/src/test/java/com/kh/spring21/Test05Ready.java
+++ spring21pay/src/test/java/com/kh/spring21/Test05Ready.java
@@ -30,7 +30,8 @@ public class Test05Ready {
 				.build();
 		
 		KakaoPayReadyResponseVO response = kakaoPayService.ready(request);
-		log.debug("response = {}", response);
+		//log.debug("response = {}", response);
+		
 	}
 
 }
